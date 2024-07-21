332c05ce87d8994ddff5b124bb8d76b9618ce8d3
jesusrobot0 <jesusrobot9000@icloud.com>
2024-07-20 19:35:50 -0500

    feat: serve static files from /public directory

--- index.js
+++ index.js
@@ -3,11 +3,14 @@ const express = require("express");
 // Crear el servidor de express
 const app = express();
 
+// Directorio público
+app.use(express.static("public"));
+
 // Rutas
-app.get("/", (req, res) => {
-  console.log("Ruta '/' ejecutada");
-  res.json({ hola: "mundo" });
-});
+// app.get("/", (req, res) => {
+//   console.log("Ruta '/' ejecutada");
+//   res.json({ hola: "mundo" });
+// });
 
 // Escuchar peticiones al servidor
 app.listen(4000, () => {
